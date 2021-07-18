b98b3e320d6ef4b74ecae888813e92b972155602
satuluri vijayalakshmi <45935821+satulurivijayalakshmi@users.noreply.github.com>
2021-07-18 16:00:36 +0530

    Add files via upload

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,63 +1,47 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler  
+from sklearn.preprocessing import StandardScaler
 import pickle
 from flask_cors import CORS
 from flask import Flask,request,render_template
 app=Flask(__name__)
 CORS(app)
-model = pickle.load(open('upload/model.pkl', 'rb'))
-@app.route('/')
-def home():
-    return render_template('main.html')
-
+model=pickle.load(open('upload\model.pkl','rb'))
 @app.route('/predict',methods=['POST'])
 def predict():
-    age=request.json['Age']
-    bp=request.json['Blood_Pressure']
-    sg=request.json['Specific_Gravity']
-    al=request.json['Albumin']
-    su=request.json['Sugar']
-    rc=request.json['Red_Blood_Cells']
-    pc=request.json['Pus_Cells']
-    pcc=request.json['Pus_Cell_Clumps']
-    ba=request.json['Bacteria']
-    bgr=request.json['Blood_Glucose_Random']
-    bu=request.json['Blood_Urea']
-    sc=request.json['Serum_Creatinine']
-    sod=request.json['Sodium']
-    pot=request.json['Potassium']
-    hemo=request.json['Hemoglobin']
-    pcv=request.json['Packed_Cell_Volume']
-    wbc=request.json['White_Blood_Cell_Count']
-    rbc=request.json['Red_Blood_Cell_Count']
-    htn=request.json['Hypertension']
-    dm=request.json['Diabetes_Milletus']
-    cad=request.json['Coronory_Artery_Disease']
-    appet=request.json['Appetite']
-    pe=request.json['Pedal_Edema']
-    ane=request.json['ane']
-    datavalues=[[age,bp,sg,al,su,rc,pc,pcc,ba,bgr,bu,sc,sod,pot,hemo,pcv,wbc,rbc,htn,dm,cad,appet,pe,ane]]
-    data=pd.DataFrame(datavalues,columns=['Age','Blood_Pressure','Specific_Gravity','Alubmin','Sugar','Red_Blood_Cells',
-                                          'Pus_Cells','Pus_Cell_Clumps','Bacteria','Blood_Glucose_Random','Blood_Urea',
-                                          'Serum_Creatinine','Sodium','Potassium','Hemoglobin','Packed_Cell_Volume',
-                                          'White_Blood_Cell_Count','Red_Blood_Cell_Count','Hypertension','Diabetes_Milletus',
-                                          'Coronory_Artery_Disease','Appetite','Pedal_Edema','ane'])
-    categorical=['Red_Blood_Cells','Pus_Cells','Pus_Cell_Clumps','Bacteria','Hypertension','Diabetes_Milletus','Coronory_Artery_Disease','Appetite','Pedal_Edema','ane']
-    from sklearn.preprocessing import LabelEncoder
-    le=LabelEncoder()
-    for i in categorical:
-        data[i] = le.fit_transform(data[i])
-    data[['Age','Blood_Pressure','Specific_Gravity','Alubmin','Sugar','Blood_Glucose_Random','Blood_Urea','Serum_Creatinine','Sodium','Potassium',
-          'Hemoglobin','Packed_Cell_Volume','White_Blood_Cell_Count','Red_Blood_Cell_Count','Hypertension']] = StandardScaler().fit_transform(data[['Age','Blood_Pressure','Specific_Gravity','Alubmin','Sugar','Blood_Glucose_Random','Blood_Urea','Serum_Creatinine','Sodium','Potassium',
-          'Hemoglobin','Packed_Cell_Volume','White_Blood_Cell_Count','Red_Blood_Cell_Count','Hypertension']])
-    res=model.predict(data)
-    output=res[0]
     
-    if output==0:
-        res_str="Chronic Kidney Disease is positive" 
-    else:
-        res_str="Chronic Kidney Disease is negative"
-    return res_str
+    age=request.json['age']
+    bloodpressure = request.json['bp']
+    specificgravity = request.json['sg']
+    albumin = request.json['al']
+    sugar = reuest.json['su']
+    redbloodcells = request.json['rbc']
+    puscells = request.json['pc']
+    puscellclumps = request.json['pcc']
+    bacteria = request.json['ba']
+    bloodglucoserandom = request.json['bgr']
+    bloodurea = request.json['bu']
+    serumcreatinine = request.json['sc']
+    sodium = request.json['sod']
+    potassium = request.json['pot']
+    hemoglobin = request.json['hemo']
+    packedcellvolume = request.json['pcv']
+    whitebloodcellcount = request.json['wc']
+    redbloodcellcount = request.json['rc']
+    hypertension = request.json['htn']
+    diabetesmilletus = request.json['dm']
+    coronoryarterydisease =request.json['cad']
+    appetite = request.json['appet']
+    pedaledema = request.json['pe']
+    anemia = request.json['ane']
+    data=pd.DataFrame(datavalues,columns=['age','bp','sg','al','su','rc','pc','pcc','ba','bgr','bu','sc','sod','pot','hemo','pcv','wbc','rbc','htn','dm','cad','appet','pe','ane'])
+    res=model.predict(data)
+    output= res[0]
 
+    if output == 0:
+        res_str = "chronic kidney disease is positive"
+    else:
+        res_str = "chronic kidney disease is negative"
+    return res_str
 if __name__=="__main__":
     app.run(debug=True)
+    
